65931743fc02130d49791ff536be8cf7efcdaeb3
Your Name <you@example.com>
2021-03-24 22:42:12 +0600

    ok

--- blog/models.py
+++ blog/models.py
@@ -3,14 +3,15 @@ from django.contrib.auth.models import User
 from ckeditor.fields import RichTextField
 from django.utils.text import slugify
 
+
 # Create your models here.
 
 class Post(models.Model):
     date = models.DateField(auto_now=False, auto_now_add=True)
-    title = models.CharField(max_length=250) 
+    title = models.CharField(max_length=20) 
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     body = RichTextField(blank=True, null=True)
-    key = models.CharField(max_length=250, unique=True)
+    key = models.CharField(max_length=20, unique=True)
     slug = models.SlugField(unique=True, null=True, blank=True, allow_unicode=True)
     
     def __str__(self):
@@ -31,12 +32,20 @@ class IP(models.Model):
 
     
 class Information(models.Model):
-    name = models.CharField(max_length=100)
-    email = models.EmailField(max_length=150)
+    name = models.CharField(max_length=50)
+    name2 = models.CharField(max_length=50)
+    email = models.EmailField(max_length=100)
     profation = models.CharField(max_length=150)
     age = models.IntegerField()
     phone = models.IntegerField()
-
+    country = models.CharField(max_length=50)
+    distric = models.CharField(max_length=50)
+    gila = models.CharField(max_length=50)
+    thana = models.CharField(max_length=50)
+    address = models.CharField(max_length=50)
+    relegion = models.CharField(max_length=30)
+  
+    
     def __str__(self):
         return self.name
 
