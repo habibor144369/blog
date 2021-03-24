32ce9866c0f5f65dba1ff871cca2f5233bf1988c
Your Name <you@example.com>
2021-03-25 00:39:34 +0600

    ok

--- blog/admin.py
+++ blog/admin.py
@@ -3,7 +3,20 @@ from.models import Post, IP, Information
 
 # Register your models here.
 
-admin.site.register(Post)
+
+
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author', 'date', 'slug', 'key']
+    list_per_page = 15
+    search_fields = ['title']
+    list_filter = ['title', 'date', 'key']
+
+class Details(admin.ModelAdmin):
+    list_display = ['name', 'name2', 'email', 'profation', 'country', 'relegion']
+    search_fields = ['name', 'email']
+    list_filter = ['name', 'email', 'address', 'phone']
+
+admin.site.register(Post, PersonAdmin)
 admin.site.register(IP)
-admin.site.register(Information)
+admin.site.register(Information, Details)
 
